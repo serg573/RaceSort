5cfdffa8d845d342f989ec7f556f57cb79193cf2
Sergey Vasiliev <serg573@gmail.com>
2017-07-10 11:46:32 +0300

    getListByRandomWithoutDuplicates() added

--- src/serg/RaseSort.java
+++ src/serg/RaseSort.java
@@ -2,9 +2,7 @@ package serg;
 
 import serg.sort_variants.BubbleSort;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by Sergey Vasiliev on 7/8/17.
@@ -15,8 +13,9 @@ public class RaseSort {
 
     public static void main(String[] args) {
 
-        //List<Integer> list = getListByRandome();
-        List<Integer> initialList = getListByMix();
+        //List<Integer> initialList = getListByRandome();
+        List<Integer> initialList = getListByRandomWithoutDuplicates();
+        //List<Integer> initialList = getListByMix();
 
         System.out.println("Unsorted:"+initialList);
 
@@ -30,6 +29,17 @@ public class RaseSort {
         List<Integer> list = new ArrayList<>(ARRAY_SIZE);
         for (int i=0; i<ARRAY_SIZE; i++)
             list.add((int) (Math.random()*(10+1)));
+
+        return list;
+    }
+
+    private static List<Integer> getListByRandomWithoutDuplicates() {
+        Set<Integer> set = new HashSet<>();
+        List<Integer> list = new ArrayList<>(ARRAY_SIZE); // If I used "return new ArrayList<>(set);" instead, it would return sorted list but I need unsorted one
+        while (set.size()<ARRAY_SIZE) {
+            int ii = (int) (Math.random()*(20+1));
+            if (set.add(ii)) list.add(ii);
+        }
 
         return list;
     }
